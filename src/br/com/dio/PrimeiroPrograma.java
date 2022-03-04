5fdcce1de10c228fde5a90c93b43b7e1bf7ae269
Vanessa_Barros <vanbarros.mvp4@gmail.com>
2022-03-04 13:45:11 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro(nome: "O problema dos 3 corpos", npag: 300);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello world!" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
